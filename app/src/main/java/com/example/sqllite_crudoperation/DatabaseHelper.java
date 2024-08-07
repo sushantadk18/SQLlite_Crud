4a887daeb452cc22780b80f3fe8d44bed5c77c4b
Sushant Adhikari <sushantadk18@gmail.com>
2024-08-07 08:01:19 +0545

    Database Update

--- app/src/main/java/com/example/sqllite_crudoperation/DatabaseHelper.java
+++ app/src/main/java/com/example/sqllite_crudoperation/DatabaseHelper.java
@@ -9,6 +9,7 @@ import androidx.annotation.Nullable;
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME="student.db";
     public static final String TABLE_NAME="student_table";
+    public static final String ID="ID";
     public static final String COL1="name";
     public static final String COL2="age";
     public static final String COL3="msg";
@@ -20,11 +21,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-    sqLiteDatabase.execSQL("CREATE TABLE"=+ TABLE_NAME+"COL1"git init);
+
+        String query="CREATE TABLE "+TABLE_NAME+"("+ID+"INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                COL1 +"TEXT NOT NULL,"+
+                COL2 +"TEXT NOT NULL,"+
+                COL3 +"TEXT NOT NULL)";
+      sqLiteDatabase.execSQL(query);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL(" DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
 
     }
 }
